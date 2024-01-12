9159ea040e7dac6549eb0bb23a5ebcc9daaf74e0
Szymon W. <23wilk.szymon@gmail.com>
2024-01-13 00:39:09 +0100

    all inputs update card

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
 //errors
 //cannot be blank
 //only numbers can only be numbers
+// \b[1-9]\b only single digit number regex
 //
-const errorElement = document.createElement("p");
+const errorBlank = document.createElement("p");
+// errorBlank.classList.add("error").innerHTML = "Error: value cannot be blank";
+//input.classList.add("error-border")
+//input.parentElement.insertBefore(errorElement, numberInput.nextSibling);
+
 
 const numberInput = document.body.querySelector("#number");
-const dateInput = document.body.querySelector("#date");
+const monthInput = document.body.querySelector("#month");
+const yearInput = document.body.querySelector("#year");
 const cvcInput = document.body.querySelector("#cvc");
 const nameInput = document.body.querySelector("#name");
-console.log(numberInput, dateInput, cvcInput, nameInput);
+const form = document.querySelector("form");
+const button = document.querySelector('button')
 
 numberInput.addEventListener("input", cardNumberChange);
-// numberInput.addEventListener("input", dateChange);
-// numberInput.addEventListener("input", cvcChange);
-// numberInput.addEventListener("input", nameChange);
+monthInput.addEventListener("input", monthChange);
+yearInput.addEventListener("input", yearChange);
+cvcInput.addEventListener("input", cvcChange);
+nameInput.addEventListener("input", nameChange);
 
-function cardNumberChange(event) {
+function cardNumberChange() {
     const number = document.querySelector("h1");
-    const form = document.querySelector("form");
     number.innerHTML = numberInput.value;
-    if (number.innerHTML.length == 17) {
-        errorElement.innerHTML = "Error: Card number should be 16 digits";
-        errorElement.classList.add("error");
-        numberInput.classList.add("error-border");
-        numberInput.parentElement.insertBefore(errorElement, numberInput.nextSibling);
-    } else if (number.innerHTML.length < 17) {
-        form.querySelectorAll('p').forEach(element => {
-            element.remove();
-        })
-        numberInput.classList.remove("error-border");
-    } else if (number.innerHTML.length >= 22) {
-        numberInput.setAttribute("disabled", "");
-        setTimeout(() => numberInput.removeAttribute("disabled"), 2000);
-    }
     //ADD WHITE SPACES EVERY 4 DIGITS using regex
-    //maybe figure out how to disable input of everything besides backspace 
-    //to lock instead of disabling at line 29
+    // string.value to array to splice(4) and join() together
+    //only allow numbers
 };
+function monthChange() {
+    const month = document.querySelector(".month");
+    month.innerHTML = monthInput.value;
+    if (month.innerHTML > 12) {
+        monthInput.value = 12;
+    } 
+    month.innerHTML = monthInput.value;
+};
+function yearChange() {
+    const year = document.querySelector(".year");
+    let thisYear = new Date().getFullYear();
+    thisYear = thisYear.toString().slice(2, 4);
+    thisYear = Number(thisYear) + 20;
+    year.innerHTML = yearInput.value;
+    if (year.innerHTML > thisYear) {
+        yearInput.value = thisYear;
+        year.innerHTML = yearInput.value;
+    };
+};
+
+function cvcChange() {
+    const cvc = document.querySelector(".cvc");
+    cvc.innerHTML = cvcInput.value;
+}
+
+function nameChange() {
+    const name = document.querySelector(".card-name");
+    name.innerHTML = nameInput.value;
+}
+
+function buttonSubmit() {
+
+}
