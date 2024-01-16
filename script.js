781db369a74206eabdc2011aeb90f28135867b83
Szymon W. <23wilk.szymon@gmail.com>
2024-01-16 22:21:53 +0100

    przerwa for research

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-//errors
-//cannot be blank
-//only numbers can only be numbers
-// \b[1-9]\b only single digit number regex
-//
-
-
-
-
 const numberInput = document.body.querySelector("#number");
 const monthInput = document.body.querySelector("#month");
 const yearInput = document.body.querySelector("#year");
 const cvcInput = document.body.querySelector("#cvc");
 const nameInput = document.body.querySelector("#name");
+const inputsContainer = document.querySelectorAll(".container")
+
 const form = document.querySelector("form");
 const button = document.querySelector('button')
-const inputs = [
+
+const numberHTML = document.querySelector(".card-number");
+const monthHTML = document.querySelector(".month");
+const yearHTML = document.querySelector(".year");
+const cvcHTML = document.querySelector(".cvc");
+const nameHTML = document.querySelector(".card-name");
+let thisYear = new Date().getFullYear();
+thisYear = thisYear.toString().slice(2, 4);
+thisYear = Number(thisYear) + 20;
+
+const inputRequirements = [
     {input: nameInput},
     {input: numberInput, requirements: 'card'},
     {input: monthInput, requirements: 'date'},
     {input: yearInput, requirements: 'date'},
     {input: cvcInput, requirements: 'cvc'}
 ]
+
 button.addEventListener("click", buttonSubmit);
 numberInput.addEventListener("input", cardNumberChange);
 monthInput.addEventListener("input", monthChange);
@@ -29,60 +33,64 @@ cvcInput.addEventListener("input", cvcChange);
 nameInput.addEventListener("input", nameChange);
 
 function cardNumberChange() {
-    const number = document.querySelector("h1");
-    number.innerHTML = numberInput.value;
+    
+    if (numberInput.value.length === 0) {
+        numberHTML.innerHTML = numberHTML.getAttribute("data-placeholder");  
+    } else {
+        numberHTML.innerHTML = numberInput.value;
+    }
     //ADD WHITE SPACES EVERY 4 DIGITS using regex
     // string.value to array to splice(4) and join() together
     //only allow numbers
-};
+}
+
 function monthChange() {
-    const month = document.querySelector(".month");
-    month.innerHTML = monthInput.value;
-    if (month.innerHTML > 12) {
+    monthHTML.innerHTML = monthInput.value;
+    if (monthHTML.innerHTML > 12) {
         monthInput.value = 12;
-    } 
-    month.innerHTML = monthInput.value;
-};
+        monthHTML.innerHTML = monthInput.value;
+    }  else if (month.value.length === 0) {
+        monthHTML.innerHTML = monthHTML.getAttribute("data-placeholder");
+    }
+}
+
+
 function yearChange() {
-    const year = document.querySelector(".year");
-    let thisYear = new Date().getFullYear();
-    thisYear = thisYear.toString().slice(2, 4);
-    thisYear = Number(thisYear) + 20;
-    year.innerHTML = yearInput.value;
-    if (year.innerHTML > thisYear) {
+    yearHTML.innerHTML = yearInput.value;
+    if (yearHTML.innerHTML > thisYear) {
         yearInput.value = thisYear;
-        year.innerHTML = yearInput.value;
-    };
-};
+        yearHTML.innerHTML = yearInput.value;
+    } else if (yearInput.value.length === 0) {
+        yearHTML.innerHTML = yearHTML.getAttribute("data-placeholder");
+    }
+}
 
 function cvcChange() {
-    const cvc = document.querySelector(".cvc");
-    cvc.innerHTML = cvcInput.value;
+    if (cvcInput.value.length === 0) {
+        cvcHTML.innerHTML = cvcHTML.getAttribute("data-placeholder");
+    } else {
+    cvcHTML.innerHTML = cvcInput.value;
+    }
 }
 
 function nameChange() {
-    const name = document.querySelector(".card-name");
-    name.innerHTML = nameInput.value;
+    if (!nameInput.value.length) {
+        nameHTML.innerHTML = nameHTML.getAttribute("data-placeholder");    
+    } else {
+    nameHTML.innerHTML = nameInput.value;
+    }
 }
-
-const errorBlank = document.querySelectorAll(".error");
+// const error = document.createElement('p')
+// error.classList.add('error');
+// error.innerHTML = `Can't be blank`;
+// error.style.display = 'block';
+// const clonedError = error.cloneNode(true);
 
 function buttonSubmit(e) {
-    document.querySelectorAll('.input-group').forEach(input => {
-        input.classList.remove("has-error");
-        if (!input.value) input.classList.add("error-border");
-    })
+    e.preventDefault();
+    inputsContainer.forEach((inputContainer) => {
+        if (inputContainer.querySelector("input").value.length === 0) {
+            inputContainer.classList.add("has-error");
+        }
+    });
 }
-//for loop function, too many problems cannot maintain it properly bugs and errors
-// let i = 0;
-// for (input of inputs) {
-//     if(!input.value) {
-//         e.preventDefault();
-//         console.log(i)
-//         errorBlank[i].style.display = "block";
-//         input.classList.add('error-border')
-//         i++
-//     } else if (input.value) {
-//         input.classList.remove("error-border");
-//     }
-// }
