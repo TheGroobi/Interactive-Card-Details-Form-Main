92a167e80e456071deb30f10e6db3151ef1f0155
Szymon W. <23wilk.szymon@gmail.com>
2024-01-17 07:22:12 +0100

    closer than before

--- script.js
+++ script.js
@@ -3,27 +3,31 @@ const monthInput = document.body.querySelector("#month");
 const yearInput = document.body.querySelector("#year");
 const cvcInput = document.body.querySelector("#cvc");
 const nameInput = document.body.querySelector("#name");
-const inputsContainer = document.querySelectorAll(".container")
 
 const form = document.querySelector("form");
-const button = document.querySelector('button')
+const button = document.querySelector('button');
+const cardNumberPattern = /[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}/
+const numberPattern = /\D/g
+const numberPatternWithSpace = /\D\v/g
 
 const numberHTML = document.querySelector(".card-number");
 const monthHTML = document.querySelector(".month");
 const yearHTML = document.querySelector(".year");
 const cvcHTML = document.querySelector(".cvc");
 const nameHTML = document.querySelector(".card-name");
+const dateHTMLDiv = document.querySelector('.date-grid-split');
+
 let thisYear = new Date().getFullYear();
 thisYear = thisYear.toString().slice(2, 4);
 thisYear = Number(thisYear) + 20;
 
-const inputRequirements = [
-    {input: nameInput},
-    {input: numberInput, requirements: 'card'},
-    {input: monthInput, requirements: 'date'},
-    {input: yearInput, requirements: 'date'},
-    {input: cvcInput, requirements: 'cvc'}
-]
+const inputsObject = [
+    { input: nameInput},
+    { input: numberInput, card: 'requirements'},
+    { input: monthInput, date: 'requirements'},
+    { input: yearInput, date: 'requirements'},
+    { input: cvcInput, cvc: 'requirements'},
+];
 
 button.addEventListener("click", buttonSubmit);
 numberInput.addEventListener("input", cardNumberChange);
@@ -33,17 +37,22 @@ cvcInput.addEventListener("input", cvcChange);
 nameInput.addEventListener("input", nameChange);
 
 function cardNumberChange() {
-    
     if (numberInput.value.length === 0) {
-        numberHTML.innerHTML = numberHTML.getAttribute("data-placeholder");  
+        numberHTML.innerHTML = numberHTML.getAttribute("data-placeholder");
     } else {
-        numberHTML.innerHTML = numberInput.value;
+        numberHTML.innerHTML = cardNumberSpaceAdd(numberInput.value);
+        // numberInput.value = (numberInput.value).replace(/\d{4}(?=.)/g, '$& ')
     }
     //ADD WHITE SPACES EVERY 4 DIGITS using regex
     // string.value to array to splice(4) and join() together
-    //only allow numbers
+};
+function cardNumberSpaceAdd(cardNumber) {
+    let cardSpaceAdded = cardNumber.split("");
+    cardSpaceAdded.splice(4, 0, " ");
+    cardSpaceAdded.splice(9, 0, " ");
+    cardSpaceAdded.splice(14, 0, " ");
+    return cardSpaceAdded = cardSpaceAdded.join("");
 }
-
 function monthChange() {
     monthHTML.innerHTML = monthInput.value;
     if (monthHTML.innerHTML > 12) {
@@ -52,9 +61,7 @@ function monthChange() {
     }  else if (month.value.length === 0) {
         monthHTML.innerHTML = monthHTML.getAttribute("data-placeholder");
     }
-}
-
-
+};
 function yearChange() {
     yearHTML.innerHTML = yearInput.value;
     if (yearHTML.innerHTML > thisYear) {
@@ -63,34 +70,57 @@ function yearChange() {
     } else if (yearInput.value.length === 0) {
         yearHTML.innerHTML = yearHTML.getAttribute("data-placeholder");
     }
-}
-
+};
 function cvcChange() {
     if (cvcInput.value.length === 0) {
         cvcHTML.innerHTML = cvcHTML.getAttribute("data-placeholder");
     } else {
     cvcHTML.innerHTML = cvcInput.value;
     }
-}
-
+};
 function nameChange() {
     if (!nameInput.value.length) {
         nameHTML.innerHTML = nameHTML.getAttribute("data-placeholder");    
     } else {
     nameHTML.innerHTML = nameInput.value;
     }
-}
-// const error = document.createElement('p')
-// error.classList.add('error');
-// error.innerHTML = `Can't be blank`;
-// error.style.display = 'block';
-// const clonedError = error.cloneNode(true);
+};
 
 function buttonSubmit(e) {
-    e.preventDefault();
-    inputsContainer.forEach((inputContainer) => {
-        if (inputContainer.querySelector("input").value.length === 0) {
-            inputContainer.classList.add("has-error");
+    inputsObject.forEach(inputObject => {
+        let input = Object.values(inputObject)[0];
+        let requirement = Object.keys(inputObject)[1];
+        const inputError = input.parentElement.lastElementChild;
+
+        inputError.textContent = "";
+        input.classList.remove('error-border');
+        if (!input.value.length) {
+            e.preventDefault();
+            inputError.textContent = "Can't be blank";
+            input.classList.add('error-border');
+        } else if (requirement) {
+            e.preventDefault();
+            if (requirement === 'card') {
+                if (numberPatternWithSpace.test(input.value)) {
+                    inputError.textContent = "Wrong format, numbers only";
+                    input.classList.add('error-border');
+                    // ADD REGEX FOR WHITE SPACES IN INPUT OF NUMBER CARD OR IT DOESN'T WORK
+                } else if (!cardNumberPattern.test(input.value)) {
+                    inputError.textContent = "Wrong format, cards should have 16 digits"
+                    input.classList.add('error-border');
+                }
+            } else if (requirement === 'date') {
+                if (numberPattern.test(input.value)) {
+                    inputError.textContent = "Wrong format, numbers only";
+                    input.classList.add('error-border');
+                    //fix the month breaking the error message
+                }
+            } else if (requirement === 'cvc') {
+                if (numberPattern.test(input.value) || input.value.length === 1 || input.value.length === 2) {
+                    inputError.textContent = "Wrong format, 3 or 4 numbers only";
+                    input.classList.add('error-border');
+                }
+            }
         }
     });
 }
